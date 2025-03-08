e4daba416414f570469c70aea7167a6b4fedfe5e
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-03-08 07:11:14 +0530

    Create index.ts

---
+++ Edge Function/hello world/index.ts
@@ -0,0 +1,15 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+
+console.log("Hello from Functions!");
+
+Deno.serve(async (req)=>{
+  const { name } = await req.json();
+  const data = {
+    message: `Hello ${name}!`
+  };
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+});
